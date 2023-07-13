288a6d1260a41397888d6b01e5f55b38cfc878ad
AleafNurr <alef.nurrokh@gmail.com>
2023-07-13 17:03:18 +1200

    Daily Commit

--- main.js
+++ main.js
@@ -4,17 +4,25 @@ import * as THREE from 'three';
 // import orbit controls
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+// Scene
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view
+  window.innerWidth/window.innerHeight, // aspect ratio
+  0.1, // near clipping plane
+  1000 // far clipping plane
+);
 
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.setZ(30);
+camera.position.setZ(0);
 
 // Texture
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
@@ -30,23 +38,25 @@ scene.add( torus );
 
 // Lighting
 const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5,5,5)
-const ambientLight = new THREE.AmbientLight(0xffffff) // soft white floodlight
+pointLight.position.set(100,50,5)
+const ambientLight = new THREE.AmbientLight(0xffffff) // floodlight
 scene.add(pointLight, ambientLight)
 
 // Helpers
 const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lightHelper, gridHelper)
+// const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper)
 
 // Orbit Controls
 const controls = new OrbitControls(camera, renderer.domElement)
 
 // Stars
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( {color: 0xffffff} );
-  const star = new THREE.Mesh( geometry, material );
+  const starTexture = new THREE.TextureLoader().load('star.png');
+  const star = new THREE.Mesh( 
+    new THREE.SphereGeometry(0.25, 24, 24), 
+    new THREE.MeshStandardMaterial( {map: starTexture} ) 
+  );
 
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
 
@@ -55,6 +65,24 @@ function addStar() {
 }
 
 Array(200).fill().forEach(addStar);
+
+// Scroll
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top; // get the top of the document
+  // torus rotation
+  
+
+  // camera position
+  // if( camera.position.z <= 0 ) {
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+  // }
+  // log the position of the camera
+  // console.log(camera.position.x, camera.position.y, camera.position.z);
+}
+
+document.body.onscroll = moveCamera;
 
 // Animation
 function animate() {
